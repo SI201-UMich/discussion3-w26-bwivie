092d4620817f1ca8e9d9b84e5af6328e806218ad
Brandon Wivietsky <brandonwivietsky@Brandons-MacBook-Air-3.local>
2026-02-03 14:59:47 -0500

    first draft

--- Discussion3.py
+++ Discussion3.py
@@ -1,29 +1,21 @@
 import math
 
-class Rectangle():
-    # Create the constructor "__init__" method
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-    # YOUR CODE HERE
-    CHANGES
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
+    def area_calculator(self):
+        return float(self.width * self.height)
 
-    # Create the "__str__" method
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        return self.width == other.width and self.height == other.height
 
-    # YOUR CODE HERE
-
-
-
-    # Create the "area_calculator" method
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "__eq__" method
-    # 
-    # Returns a boolean value
-
-    # YOUR CODE HERE
 
 
     
